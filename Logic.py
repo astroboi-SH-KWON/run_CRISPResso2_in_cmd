5646da2cca3c7753ad24652dffc5f86577bcc3b4
astroboi-lab <terry007x@korea.ac.kr>
2021-05-27 09:43:58 +0900

    change file name "run_CRISPResso2" to "CRISPResso2_commands"

--- Logic.py
+++ Logic.py
@@ -1,11 +1,12 @@
 
-import run_CRISPResso2
+import CRISPResso2_commands
+
 class Logics:
     def __init__(self):
         self.tmp = ""
 
     def run_crispresso_w_list(self, input_list):
         print "run_crispresso_w_list START"
-        run_crispresso = run_CRISPResso2.run_CRISPResso2()
+        run_crispresso = CRISPResso2_commands.run_CRISPResso2()
         for init in input_list:
             run_crispresso.run_CRISPResso_fastq_r1(init)
